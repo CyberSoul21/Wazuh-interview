32f8598c6da69082194e4a5b3536b67be2466dad
wjalmarior <wilson.jar21@gmail.com>
2025-03-30 12:49:02 -0500

    Expecting arguments from terminal

--- Task1/src/main.cpp
+++ Task1/src/main.cpp
@@ -1,14 +1,6 @@
-//#include <iostream>
 #include "../include/monty_hall.h"
 #include <thread>
-//#include <atomic>
 
-//using namespace std;
-
-
-
-int main()
-{
     /**
      * @brief 
      * TODO: 
@@ -20,14 +12,19 @@ int main()
      * 6- Test cases
      * 
      */
-    const int total_simulations = 5000000;
+
+
+int main(int argc, char* argv[])
+{
+    std::cout <<"Running simulation... "<<endl;
+
+    const int total_simulations = 1000;
     monty_hall game_stay;
-    //monty_hall game_switch;    
-    
+    monty_hall game_switch; 
     std::atomic<int> wins(0); // Atomic variable for thread-safe updates
 
     // Measure time for non-parallel program
-    auto start = std::chrono::high_resolution_clock::now();
+    //auto start = std::chrono::high_resolution_clock::now();
 
     game_stay.stayStrategySimulation_chunk(total_simulations,std::ref(wins));
     std::cout <<"Games wons with stay strategy: "<<wins.load()<<endl;
@@ -37,10 +34,10 @@ int main()
     //std::cout <<"Games wons with switch strategy: "<<wins.load()<<endl;
     //wins.store(0);
 
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration_stay = end - start;
+    //auto end = std::chrono::high_resolution_clock::now();
+    //std::chrono::duration<double> duration_stay = end - start;
     // Output the time taken for each strategy
-    std::cout << "Time taken running sequentially: " << duration_stay.count() << " seconds" << std::endl;
+    //std::cout << "Time taken running sequentially: " << duration_stay.count() << " seconds" << std::endl;
 
 
     //game_stay.reset();
@@ -53,7 +50,7 @@ int main()
     //std::atomic<int> wins(0); // Atomic variable for thread-safe updates
     
     // Measure time for non-parallel program
-    start = std::chrono::high_resolution_clock::now();
+    //start = std::chrono::high_resolution_clock::now();
 
     // Create threads to run the simulation in parallel
     for (int i = 0; i < num_threads; i++)
@@ -71,10 +68,10 @@ int main()
     std::cout << "Games won: " << wins.load() << std::endl;
 
 
-    end = std::chrono::high_resolution_clock::now();
-    duration_stay = end - start;
+    //end = std::chrono::high_resolution_clock::now();
+    //duration_stay = end - start;
     // Output the time taken for each strategy
-    std::cout << "Time taken running in parallel: " << duration_stay.count() << " seconds" << std::endl;
+    //std::cout << "Time taken running in parallel: " << duration_stay.count() << " seconds" << std::endl;
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
 
