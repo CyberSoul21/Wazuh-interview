4330e24bf1b8fa3995dceaf2a23bc768b9a910a9
wjalmarior <wilson.jar21@gmail.com>
2025-03-30 13:26:08 -0500

    Adding condition for the arguments

--- Task1/src/main.cpp
+++ Task1/src/main.cpp
@@ -18,21 +18,76 @@ int main(int argc, char* argv[])
 {
     std::cout <<"Running simulation... "<<endl;
 
-    const int total_simulations = 1000;
+    int total_simulations = 0;
+    bool stay = false;
+    bool change = false;
+    bool both = false;
     monty_hall game_stay;
     monty_hall game_switch; 
     std::atomic<int> wins(0); // Atomic variable for thread-safe updates
+    std::vector<std::thread> threads;
+
+
+    const int num_threads = std::thread::hardware_concurrency(); // Get number of hardware threads available
+    const int simulations_per_thread = total_simulations / num_threads;
 
     // Measure time for non-parallel program
     //auto start = std::chrono::high_resolution_clock::now();
+    
+    //Default simulation without arguments
+    if(argc == 1)
+    {
+        total_simulations = 100000;
+        game_stay.stayStrategySimulation_chunk(total_simulations,std::ref(wins));
+        std::cout <<"Games wons with stay strategy: "<<wins.load()<<endl;
+        wins.store(0);
 
-    game_stay.stayStrategySimulation_chunk(total_simulations,std::ref(wins));
-    std::cout <<"Games wons with stay strategy: "<<wins.load()<<endl;
-    wins.store(0);
+        game_switch.switchStrategySimulation_chunk(total_simulations,std::ref(wins));
+        std::cout <<"Games wons with switch strategy: "<<wins.load()<<endl;
+        wins.store(0);
+    }
+    else if(argc == 3)
+    {
+        // First argument (string)
+        std::string strategy = argv[1];  
+        
+        //Converte second argument to an integer
+        try 
+        {
+            total_simulations = std::stoi(argv[2]);  // Convert the second argument to an integer
+        } 
+        catch (const std::invalid_argument& e) 
+        {
+            std::cout << "Invalid argument: the second argument must be an integer." << std::endl;
+            return 1;
+        } 
+        catch (const std::out_of_range& e) 
+        {
+            std::cout << "Out of range: the integer is too large." << std::endl;
+            return 1;
+        }
 
-    //game_switch.switchStrategySimulation_chunk(total_simulations,std::ref(wins));
-    //std::cout <<"Games wons with switch strategy: "<<wins.load()<<endl;
-    //wins.store(0);
+        if(strategy == "stay")
+        {
+            stay = true;
+        }
+        else if(strategy == "switch")
+        {
+            change = true;
+        }
+        else if(strategy == "both")
+        {
+            both = true;
+        }
+        else
+        {
+            std::cout << "Invalid argument: the first argument must be an option: stay, switch or both." << std::endl;
+            return 1; 
+        } 
+
+    }
+
+
 
     //auto end = std::chrono::high_resolution_clock::now();
     //std::chrono::duration<double> duration_stay = end - start;
@@ -43,11 +98,8 @@ int main(int argc, char* argv[])
     //game_stay.reset();
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
-    const int num_threads = std::thread::hardware_concurrency(); // Get number of hardware threads available
-    const int simulations_per_thread = total_simulations / num_threads;
 
-    std::vector<std::thread> threads;
-    //std::atomic<int> wins(0); // Atomic variable for thread-safe updates
+
     
     // Measure time for non-parallel program
     //start = std::chrono::high_resolution_clock::now();
@@ -68,10 +120,7 @@ int main(int argc, char* argv[])
     std::cout << "Games won: " << wins.load() << std::endl;
 
 
-    //end = std::chrono::high_resolution_clock::now();
-    //duration_stay = end - start;
-    // Output the time taken for each strategy
-    //std::cout << "Time taken running in parallel: " << duration_stay.count() << " seconds" << std::endl;
+
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
 
